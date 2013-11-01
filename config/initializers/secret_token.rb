14474766ab1daca3be6cc7cd1159e9a73a714b75
Charlie Gaines <beaugaines@gmail.com>
2013-11-01 10:44:46 -0400

    vergin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = 'dd9971fcd66eb9dbc60b819fdb8cd34eecb2aeab07afcfa1588787d49e787e20f6ffd055105f362a4dc18f4afda11242e3e53d62f37c9b872f45defb42f8b927'
