54337e2c33d7d48d739cfa59bc4ee96351b377d8
Charlie Gaines <beaugaines@gmail.com>
2013-11-04 16:09:22 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,20 @@
 
 require 'ffaker'
 
+u = User.create(username: 'blawkitter', email: 'guy@email.com', password: 'password')
+u2 = User.create(username: 'kittblawker', email: 'othaguy@email.com', password: 'password')
+a = User.create(username: 'admin', email: 'admin@blawkit.com', password: 'password')
+users = %w(u u2)
+users.each { |u| u.confirm! }
+
 rand(10..30).times do
-  p = Post.create(title: Faker::HipsterIpsum.words(rand(1..10)).join(" "),
+  p = u.posts.create(title: Faker::HipsterIpsum.words(rand(1..10)).join(" "),
     body: Faker::HipsterIpsum.paragraphs(rand(1..4)).join("\n"))
   rand(3..5).times do
-    p.comments.create(body: Faker::HipsterIpsum.paragraphs(rand(1..3)).join("\n"))
+    p.comments.create(body: Faker::HipsterIpsum.paragraphs(rand(1..3)).join("\n"), user_id: users.sample)
+  end
+  rand(0..1).times do
+    p.comments.create(body: 'This comment is inappropriate and will be removed.', user_id: a.id)
   end
 end
 
