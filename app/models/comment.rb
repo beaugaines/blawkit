178e50d3e580f34c66b1d282421c4d105e028cf5
Charlie Gaines <beaugaines@gmail.com>
2013-11-04 16:27:13 -0500

    seed file working niiiice

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  belongs_to :user
   attr_accessible :body, :user_id
+
+  delegate :username, to: :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,34 @@
 
 require 'ffaker'
 
-u = User.create(username: 'blawkitter', email: 'guy@email.com', password: 'password')
-u2 = User.create(username: 'kittblawker', email: 'othaguy@email.com', password: 'password')
-a = User.create(username: 'admin', email: 'admin@blawkit.com', password: 'password')
-users = %w(u u2)
-users.each { |u| u.confirm! }
+User.delete_all
+Post.delete_all
+Comment.delete_all
+
+class User
+  def skip_confirm
+    skip_confirmation!
+    save!
+  end
+end
+
+
+u = User.new(username: 'blawkitter', email: 'guy@email.com', password: 'password')
+u.skip_confirm
+
+u2 = User.new(username: 'kittblawker', email: 'othaguy@email.com', password: 'password')
+u2.skip_confirm
+
+a = User.new(username: 'admin', email: 'admin@blawkit.com', password: 'password')
+a.skip_confirm
+
+USERS = [u, u2]
 
 rand(10..30).times do
-  p = u.posts.create(title: Faker::HipsterIpsum.words(rand(1..10)).join(" "),
+  p = u.posts.create(title: Faker::HipsterIpsum.words(rand(1..10)).join(" ").titleize,
     body: Faker::HipsterIpsum.paragraphs(rand(1..4)).join("\n"))
   rand(3..5).times do
-    p.comments.create(body: Faker::HipsterIpsum.paragraphs(rand(1..3)).join("\n"), user_id: users.sample)
+    p.comments.create(body: Faker::HipsterIpsum.paragraphs(rand(1..3)).join("\n"), user_id: USERS.sample.id)
   end
   rand(0..1).times do
     p.comments.create(body: 'This comment is inappropriate and will be removed.', user_id: a.id)
