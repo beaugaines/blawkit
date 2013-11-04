b6961e86318663fd577b4a01926f52b8787ce9f8
Charlie Gaines <beaugaines@gmail.com>
2013-11-04 13:25:50 -0500

    heroku email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,10 @@ Bloccit::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # email config for heroku
+  config.action_mailer.default_url_options = { :host => 'blawkit.herokuapp.com' }  
+
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
