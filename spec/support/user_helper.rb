e8c252b59b0ab1aa45175f27b68bc7d55dc3e59d
Charlie Gaines <beaugaines@gmail.com>
2013-11-04 12:49:26 -0500

    user sign in redirects to dashboard

--- spec/support/user_helper.rb
+++ spec/support/user_helper.rb
@@ -2,8 +2,8 @@ module UserHelper
 
   def login user
     visit new_user_session_path
-    fill_in 'Email', with: user.email, match: :first
-    fill_in 'Password', with: user.password, match: :first
+    fill_in 'Email', with: user.email, match: :prefer_exact
+    fill_in 'Password', with: user.password , match: :prefer_exact
     click_button 'Sign in'
   end
 
