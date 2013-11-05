d589e214921499a2a805153d012786b5096965b9
Charlie Gaines <beaugaines@gmail.com>
2013-11-04 22:12:15 -0500

    topic cont methods

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,30 @@
 class TopicsController < ApplicationController
+  before_filter :ensure_topic, only: [:show, :edit, :update]
+
   def index
+    @topics = Topic.all
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic =Topic.new(params[:topic])
+    if @topic.save
+      redirect_to @topic, notice: 'Topic created'
+    else
+      render :new, alert: 'Topic creation failed; please try again'
+    end
+  end
+
+  def update
+    if @topic.update_attributes(params[:topic])
+      redirect_to @topic
+    else
+      flash[:error] = "Error saving topic. Please try again"
+      render :edit
+    end
   end
 
   def show
@@ -10,4 +32,17 @@ class TopicsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def ensure_topic
+    redirect_to root_path, notice: 'No such topic' unless topic
+  end
+
+  def topic
+    @topic ||= Topic.find_by_id(params[:id])
+  end
+  
+  helper_method :topic
+  
 end
