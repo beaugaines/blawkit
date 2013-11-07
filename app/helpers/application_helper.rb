ff2f36723e0e90243bb194f7eb57988e406740ec
Charlie Gaines <beaugaines@gmail.com>
2013-11-07 16:58:28 -0500

    foundatin paginate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,14 +30,15 @@ module ApplicationHelper
 
   # custom will_paginate views
 
-  def will_paginate(collection_or_options = nil, options = {})
-    if collection_or_options.is_a?(Hash)
-      options, collection_or_options = collection_or_options, nil
-    unless options[:renderer]
-      options = options.merge renderer: FoundationLinkRenderer
-    end
-    super *[collection_or_options, options].compact
-  end
+  # def will_paginate(collection_or_options = nil, options = {})
+  #   if collection_or_options.is_a?(Hash)
+  #     options, collection_or_options = collection_or_options, nil
+  #   end
+  #   unless options[:renderer]
+  #     options = options.merge renderer: FoundationLinkRenderer
+  #   end
+  #   super *[collection_or_options, options].compact
+  # end
 
 
 
