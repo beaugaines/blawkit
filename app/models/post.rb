07f31c8aa011a0a4cd84d4af9ec479537d14874b
Charlie Gaines <beaugaines@gmail.com>
2013-11-11 21:56:36 -0500

    vote up and down vote methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,12 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+  
+  def down_votes
+    votes.where(value: -1).count
+  end
 end
