b1109af8476ff9da0e8cf88bbe754e42e43f3bf6
Charlie Gaines <beaugaines@gmail.com>
2013-11-11 21:55:41 -0500

    vote model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131107040715) do
+ActiveRecord::Schema.define(:version => 20131112025152) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -70,5 +70,16 @@ ActiveRecord::Schema.define(:version => 20131107040715) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "value",      :null => false
+    t.integer  "user_id",    :null => false
+    t.integer  "post_id",    :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "votes", ["post_id"], :name => "index_votes_on_post_id"
+  add_index "votes", ["user_id"], :name => "index_votes_on_user_id"
 
 end
