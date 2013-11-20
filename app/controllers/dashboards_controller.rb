d7d99de8fefc0746f155fd14c76dd4be9cc342f9
Charlie Gaines <beaugaines@gmail.com>
2013-11-20 15:30:34 -0500

    added includes topic to @posts on user dboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @dashboard = Dashboard.new(current_user)
-    @posts = Post.visible_to(current_user).in_last_week.paginate(page: params[:page])
+    @posts = Post.visible_to(current_user).in_last_week.paginate(page: params[:page]).includes(:topic)
   end
 end
